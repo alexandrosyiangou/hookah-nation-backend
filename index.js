a604a67e4d01413cb6cc537f4add9981fa967cb4
Alexandros Yiangou <alexyiangousolutions@gmail.com>
2020-01-27 13:32:52 +0200

    Minor start server fixes

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const morgan = require('morgan')
 const bodyParser = require('body-parser')
 
 const BrandRoutes = require('./routes/Brand')
@@ -7,6 +8,7 @@ const TobaccoRoutes = require('./routes/Tobacco')
 const app = express()
 
 const port = process.env.PORT || 3000
+process.env.NODE_ENV = process.env.NODE_ENV == undefined ? 'development' : process.env.NODE_ENV 
 
 process.on('uncaughtException', (e) => console.error('Process Unhandled Error', e))
 process.on('unhandledRejection', (e) => console.error('Process Unhandled Rejection', e))
@@ -21,4 +23,8 @@ app.use('/brands', BrandRoutes)
 
 app.use('/tobaccos', TobaccoRoutes)
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => {
+  console.log(`Running server in ${process.env.NODE_ENV} mode`)
+  console.log(`Server listening on 127.0.0.1:${port}`)
+})
+
--- knex/index.js
+++ knex/index.js
@@ -4,4 +4,4 @@ const knexDev = require('knex')(config.knex.development)
 
 const knexProd = require('knex')(config.knex.production)
 
-exports.knex = process.env.NODE_ENV = 'development' ? knexDev : knexProd
+exports.knex = process.env.NODE_ENV == 'development' ? knexDev : knexProd
