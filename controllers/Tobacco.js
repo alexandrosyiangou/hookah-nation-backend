58678121165eaf68c68ea964e1380ab3c052c256
Alexandros Yiangou <alexyiangousolutions@gmail.com>
2020-02-16 17:51:39 +0200

    Tobaccos /filter endpoint

--- controllers/Tobacco.js
+++ controllers/Tobacco.js
@@ -1,11 +1,33 @@
 const Tobacco = require('../models/Tobacco')
 const knex = require('../knex')
 
+const tobaccoTable = knex.knex('tobaccos')
+
 /** 
  * @returns {Tobacco[]} List of all [Tobacco] objects
  */
 async function getAllTobaccos() {
-  return await knex.knex('tobaccos').select()
+  return await tobaccoTable.select()
+}
+
+async function getTobaccosByBrand(brand) {
+  return await tobaccoTable.select().where('brand', brand)
+}
+
+async function getTobaccosByCategory(category) {
+  return await tobaccoTable.select().where('category', category)
+}
+
+async function getTobaccosBySubCategory(subCategory) {
+  return await tobaccoTable.select().where('sub_category', subCategory)
+}
+
+async function getTobaccosByTaste(taste) {
+  return await tobaccoTable.select().where('taste', taste)
 }
 
 exports.getAllTobaccos = getAllTobaccos
+exports.getTobaccosByBrand = getTobaccosByBrand
+exports.getTobaccosByCategory = getTobaccosByCategory
+exports.getTobaccosBySubCategory = getTobaccosBySubCategory
+exports.getTobaccosByTaste = getTobaccosByTaste
--- routes/Tobacco.js
+++ routes/Tobacco.js
@@ -1,8 +1,31 @@
 const TobaccoRouter = require('express').Router()
-const {getAllTobaccos} = require('../controllers/Tobacco')
+const TobaccoController = require('../controllers/Tobacco')
 
 TobaccoRouter.get('/all', async (req, res) => {
-  res.json(await getAllTobaccos())
+  res.json(await TobaccoController.getAllTobaccos())
+})
+
+TobaccoRouter.get('/filter', async (req, res) => {
+  const {query} = req
+  let filterResults
+  if (req.url.includes('brand')) {
+    filterResults = await TobaccoController.getTobaccosByBrand(query.brand)
+    console.log(`[TobaccoRouter] ${query.brand} filter results`)
+  } else if (req.url.includes('category')) {
+    filterResults = await TobaccoController.getTobaccosByCategory(query.category)
+    console.log(`[TobaccoRouter] ${query.category} filter results`)
+  } else if (req.url.includes('sub_category')) {
+    filterResults = await TobaccoController.getTobaccosBySubCategory(query.sub_category)
+    console.log(`[TobaccoRouter] ${query.sub_category} filter results`)
+  } else if (req.url.includes('taste')) {
+    filterResults = await TobaccoController.getTobaccosByTaste(query.taste)
+    console.log(`[TobaccoRouter] ${query.taste} filter results`)
+  } else {
+    filterResults = ''
+    console.log('[TobaccoRouter] Unknown Query Provided')
+  } 
+
+  res.json(filterResults)
 })
 
 module.exports = TobaccoRouter
